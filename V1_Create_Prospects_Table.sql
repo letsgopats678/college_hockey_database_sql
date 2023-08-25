e1fab6de348e716491827e49d29c513a2aced630
letsgopens87 <98929102+letsgopats678@users.noreply.github.com>
2023-08-25 10:27:57 -0400

    Create V1_Create_Prospects_Table.sql

---
+++ V1_Create_Prospects_Table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Prospects (  
+    id INT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    college VARCHAR(30) NOT NULL,
+    nhlteam VARCHAR(30) NOT NULL,
+    age INT NOT NULL,
+    position VARCHAR(30) NOT NULL,
+    draftselection VARCHAR(30)
+);
