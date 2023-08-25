4c617f49f4c43729e6514468ffac8b29b9946e61
letsgopens87 <98929102+letsgopats678@users.noreply.github.com>
2023-08-25 10:34:26 -0400

    Create DAO.java

---
+++ DAO.java
@@ -0,0 +1,19 @@
+package entity;
+import java.util.List;
+import java.util.Optional;
+/**
+ *
+ * @author Gokhan
+ * @param <T>
+ */
+public interface DAO<T> 
+{
+    //Optional is a container used to contain not-null objects.
+    //https://www.tutorialspoint.com/java8/java8_optional_class.htm
+    Optional<T> get(int id);
+    List<T> getAll();
+    void insert(T t);
+    void update(T t);
+    void delete(T t);
+    List<String> getColumnNames();
+}
